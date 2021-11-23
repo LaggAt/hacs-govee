b27e70b6b313ceeebf1ef49aa5aca6c29c8b976e
Christopher Nethercott <ccnethercott@gmail.com>
2021-11-23 14:07:40 +0000

    fix(light/attributes): round reset seconds to 2 decimal places

--- custom_components/govee/light.py
+++ custom_components/govee/light.py
@@ -310,7 +310,7 @@ class GoveeLightEntity(LightEntity):
             # rate limiting information on Govee API
             "rate_limit_total": self._hub.rate_limit_total,
             "rate_limit_remaining": self._hub.rate_limit_remaining,
-            "rate_limit_reset_seconds": self._hub.rate_limit_reset_seconds,
+            "rate_limit_reset_seconds": round(self._hub.rate_limit_reset_seconds, 2),
             "rate_limit_reset": datetime.fromtimestamp(self._hub.rate_limit_reset).isoformat(),
             "rate_limit_on": self._hub.rate_limit_on,
             # general information
