573021f3645a52b2f0129a2da399fbe48ff8fb5c
Florian Lagg <github@florian.lagg.at>
2022-04-05 10:23:37 +0000

    added rust compiler needed for pythons Cryptography package

--- .devcontainer/postCreateContainer.sh
+++ .devcontainer/postCreateContainer.sh
@@ -2,7 +2,7 @@
 
 /bin/echo ------------ postCreateCommand apt-get ------------
 apt-get update
-apt-get -y install bash bluetooth bluez bluez-tools build-essential ca-certificates cython gcc git iputils-ping libatomic1 libavcodec-dev libc-dev libffi-dev libjpeg-dev libpcap-dev libssl-dev make nano openssh-client procps python3 python3-dev python3-dev python3-pip python3-setuptools rfkill unzip wget wget zlib1g-dev
+apt-get -y install bash bluetooth bluez bluez-tools build-essential ca-certificates cargo cython gcc git iputils-ping libatomic1 libavcodec-dev libc-dev libffi-dev libjpeg-dev libpcap-dev libssl-dev make nano openssh-client procps python3 python3-dev python3-pip python3-setuptools rfkill unzip wget wget zlib1g-dev
 
 /bin/echo ------------ postCreateCommand container install ------------
 mkdir -p /src/ludeeus
